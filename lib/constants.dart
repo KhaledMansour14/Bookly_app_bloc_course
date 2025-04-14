b6c442a0aae8210a21e71945e40f4088af1dcb12
KhaledMansour14 <khaledmansoor14@gmail.com>
2025-04-14 13:41:18 +0200

    create style & add fonts

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimayColor = Color(0xff100B20);
 const kTranstionDuration = Duration(milliseconds: 250);
+const kCormorantGaramond = 'CormorantGaramond';
