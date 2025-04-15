1fc4cedb16395748e71bad30c9348dcc5a377106
KhaledMansour14 <khaledmansoor14@gmail.com>
2025-04-15 09:15:55 +0200

    create custom_book_details_app_bar

--- lib/features/home/presentation/views/widgets/book_detail_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_detail_view_body.dart
@@ -1,10 +1,20 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
 
-class BookDetailViewBody extends StatelessWidget {
-  const BookDetailViewBody({super.key});
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 26),
+        child: const Column(
+          children: [
+            CustomBookDetailsAppBar(),
+          ],
+        ),
+      ),
+    );
   }
 }
