bca93b85cf6699db945f85659b2804fd4e8149bf
KhaledMansour14 <khaledmansoor14@gmail.com>
2025-04-18 09:00:11 +0200

    create search page & end UI

---
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -0,0 +1,21 @@
+
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      padding: EdgeInsets.zero,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: const BestSellerListViewItem(),
+        );
+      },
+    );
+  }
+}
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,3 +1,6 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/search/presentation/views/widgets/custom_search_text_filed.dart';
+import 'package:bookly_app/features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -5,6 +8,21 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CustomSearchTextField(),
+          const SizedBox(height: 16),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18.copyWith(fontWeight: FontWeight.w700),
+          ),
+          const SizedBox(height: 20),
+          const Expanded(child: SearchResultListView()),
+        ],
+      ),
+    );
   }
 }
