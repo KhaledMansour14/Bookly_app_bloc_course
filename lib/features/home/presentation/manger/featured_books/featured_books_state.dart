c320a49235dc8ff73883094b0fb6a406111e5ea4
KhaledMansour14 <khaledmansoor14@gmail.com>
2025-04-21 09:46:41 +0200

    create featured books method

--- lib/features/home/presentation/manger/featured_books/featured_books_state.dart
+++ lib/features/home/presentation/manger/featured_books/featured_books_state.dart
@@ -22,3 +22,15 @@ final class FeaturedBooksSuccess extends FeaturedBooksState {
 
   const FeaturedBooksSuccess(this.books);
 }
+
+
+
+// sudo ip addr flush dev eno1
+// sudo ip addr add 192.168.1.150/24 dev eno1
+// sudo ip route add default via 192.168.1.1
+// echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+
+// sudo ip addr flush dev eno1
+// sudo ip addr add 192.168.2.12/24 dev eno1
+// sudo ip route add default via 192.168.2.1
+// echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
